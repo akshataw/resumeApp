2ca87ff2120052cf580f6947fd7c9b54cd94618c
akshataw <wagheakshu@gmail.com>
2019-01-07 16:13:21 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,5 @@
+function showResume(){
+  var myName = document.getElementById('name').value;
+  document.getElementById('username').innerHTML = Date();
+//  document.getElementById('username').innerText = myName;
+}
